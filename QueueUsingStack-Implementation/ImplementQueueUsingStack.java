d5a276f627c9fc67663b624b4a879dc7a6fe6bae
Dharani Subramaniam <dharani2597@gmail.com>
2026-04-19 19:56:04 +0530

    Day 61 ImplementQueueUsingStack

---
+++ QueueUsingStack-Implementation/ImplementQueueUsingStack.java
@@ -0,0 +1,22 @@
+class MyQueue {
+    Stack<Integer> input = new Stack<>();
+    Stack<Integer> output = new Stack<>();
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek(); // Ensure output stack has the current elements
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+}
